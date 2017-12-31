fb613ca230bb7cef1d1eb311ab0504a4c88ce3f2
allen <allen@allenPC>
2017-12-31 15:37:12 +0000

    dao and dtos still need to create a db for burger

---
+++ src/java/DAO/BurgerDAOInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DTO.Burger;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author allen
+ */
+public interface BurgerDAOInterface {
+    public ArrayList<Burger> viewAllBurgers();
+    public boolean createBurger(String breadType, String meatType, String sauce, String salad, double price);
+    
+    
+}

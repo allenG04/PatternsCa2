737d1bb3be369fa7793a91afd8dc49732ce8c830
allen <allen@allenPC>
2018-01-04 15:25:18 +0000

    burgerInterface for decorator

---
+++ src/java/DTO/Burger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author allen
+ */
+public interface Burger {
+    
+    public String getBasicBun();
+    public String getOrigBeef();
+    public String getKetchup();
+    public String getPlainLettuce();
+    public String getDescription();
+    public double getPrice();
+    
+    
+}

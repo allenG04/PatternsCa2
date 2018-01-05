4aea8d7eeaa9d5d0b30a6a3c0cdde36f0fb28ad5
allen <allen@allenPC>
2018-01-05 15:09:54 +0000

    proxy added

--- src/java/DAO/BurgerDAOProxy.java
+++ src/java/DAO/BurgerDAOProxy.java
@@ -9,6 +9,23 @@ package DAO;
  *
  * @author allen
  */
-public class BurgerDAOProxy {
+import DTO.PlainBurger;
+import java.util.ArrayList;
+
+public class BurgerDAOProxy implements BurgerDAOInterface{
+    
+    private BurgerDAO bDao;
+    @Override
+    public ArrayList<PlainBurger> viewAllBurgers() {
+        ArrayList<PlainBurger> allBurgers = bDao.viewAllBurgers();
+        return allBurgers;
+    }
+    
+    @Override
+    public boolean createBurger(int burger_id,String basicBun, String origBeef, String ketchup, String plainLettuce, String description, double price) {
+        boolean burger = bDao.createBurger(burger_id,basicBun, origBeef, ketchup, plainLettuce, description, price);
+        return burger;
+    }
+    
     
 }
